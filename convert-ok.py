cb2827b6453653d65e9efc2ed27000e536bfa408
Wachira Duangdee <wcrcorp@gmail.com>
2021-03-14 12:03:37 +0700

    Add files via upload

---
+++ convert-ok.py
@@ -0,0 +1,15 @@
+#!python
+'''
+read file for python3
+'''
+import sys
+
+def main():
+    file1 = sys.argv[1]
+    f= open(file1,"r", encoding="UTF-8")
+    for i in f:
+        x = i.split()
+        print(i,end='')
+
+if (__name__== "__main__"):
+  main()
